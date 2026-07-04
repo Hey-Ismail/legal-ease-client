96c43182c79d0f905d2962788540f5b8433aa28f
Hey-Ismail <Heythereitsfahim@gmail.com>
2026-07-04 21:56:02 +0600

    added providers for this assignment

---
+++ src/components/provider/ToastProvider.jsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { Slide, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+export default function ToastProvider() {
+    return (
+        <ToastContainer
+            position="top-center"
+            autoClose={5000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick={false}
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme="light"
+            transition={Slide}
+        />
+    );
+}
